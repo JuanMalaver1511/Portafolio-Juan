b61ed3427b379c52ccad7fb1ed81a2250c13c00d
CampusVirtualTunja <campusvirtualsolicitud@gmail.com>
2025-10-20 10:12:50 -0500

    Implement code changes to enhance functionality and improve performance

--- main.js
+++ main.js
@@ -25,14 +25,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("java"); 
-        habilidades[3].classList.add("sql"); 
-        habilidades[4].classList.add("comunicacion"); 
-        habilidades[5].classList.add("trabajo"); 
-        habilidades[6].classList.add("creatividad");
-        habilidades[7].classList.add("dedicacion");  
+        habilidades[0].classList.add("react");
+        habilidades[1].classList.add("moodle");
+        habilidades[2].classList.add("javascript");
+        habilidades[3].classList.add("htmlcss");
+        habilidades[4].classList.add("java"); 
+        habilidades[5].classList.add("sql"); 
+        habilidades[6].classList.add("comunicacion"); 
+        habilidades[7].classList.add("trabajo"); 
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");  
 
     }
 
@@ -40,4 +42,3 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 }
- // Alerta para confirmar si se envio el mensaje
